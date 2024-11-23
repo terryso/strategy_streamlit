14b8102d231cdb99905beda76c09fe3f7663284b
NEE <oxtiger@gmail.com>
2024-11-23 18:07:20 +0800

    Update kline_downloader.py

--- kline_downloader.py
+++ kline_downloader.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import argparse
 import os  # 添加导入
 
-BASE_URL = "https://api.binance.com"
+BASE_URL = "https://api.binance.us"
 REQ_LIMIT = 1000
 SUPPORT_INTERVAL = {"1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"}
 
